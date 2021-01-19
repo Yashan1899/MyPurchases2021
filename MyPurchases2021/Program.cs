cc6d9735592029f9f8be836e4d3d77cd7c8fe11f
MaxYashan <Xamax1899@gmail.com>
2021-01-19 16:36:00 +0200

    Add switch-method

--- MyPurchases2021/Program.cs
+++ MyPurchases2021/Program.cs
@@ -6,7 +6,57 @@ namespace MyPurchases2021
     {
         static void Main(string[] args)
         {
-            // switch   Add, Remove, Edit, Show, Purchased, Exit
+            MyPurchase myPurchase = new MyPurchase() 
+            { 
+                Name = "Tent", Activity = MyPurchase.KindOfActivity.Tourism, AproxPrice = 3500, Priority = 3
+            };
+
+            bool flag = true;
+            while (flag)
+            {
+                ConsoleColor color = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("1.Show  \t 2. Add  \t 3. Edit");
+                Console.WriteLine("4. Delete \t 5. Purchased \t 6. Exit");
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("Enter the number of action:");
+                Console.ForegroundColor = color;
+                try
+                {
+                    int number = Convert.ToInt32(Console.ReadLine());
+
+                    switch (number)
+                    {
+                        case 1:
+                           
+                            break;
+                        case 2:
+                           
+                            break;
+                        case 3:
+                          
+                            break;
+                        case 4:
+                           
+                            break;
+                        case 5:
+                           
+                            break;
+                        case 6:
+                            flag = false;
+                            continue;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    color = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.ForegroundColor = color;
+                }
+            }
+            
+           
             // switch2  All, only 1-priority, only 2-priority,only 3-priority
             // method sort
 
